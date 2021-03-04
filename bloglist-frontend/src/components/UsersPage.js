1df75958912b48b06fc24eb630520c741d025130
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-03-04 19:42:38 +0530

    created UsersPaje.js component

---
+++ bloglist-frontend/src/components/UsersPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import users from '../services/users'
+
+const UsersPage = () => {
+    const users = useSelector(state => state.users)
+    return (
+        <>
+            <h1>Users</h1>
+            <div>
+                <span>username</span>
+                <span>blogs created</span>
+                <ul>
+                {users?.map((user) => (
+                    <li  key={user?.id}>
+                    <Link to={`/users/${user.id}`}>
+                        <span >{user?.username}</span>
+                        <span >{user?.blogs?.length}</span>
+                    </Link>
+                    </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default UserPage
