5aea76fee32a84f611f1e0aaa10af56dc3c79304
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-03-06 12:01:28 +0530

    refactored blogs services and removed old tests

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -17,7 +17,7 @@ const create = async (newObject) => {
     headers: { Authorization: token },
   }
 
-const response = await axios.post(baseUrl, newObject, config)
+  const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
 
@@ -26,26 +26,32 @@ const update = async (id, newObject) => {
   return response.data
 }
 
-const remove = async (id) => {
+const deleteBlog = async (id) => {
   const config = {
     headers: { Authorization: token },
   }
+
   const response = await axios.delete(`${baseUrl}/${id}`, config)
   return response.data
 }
 
 const addComment = async (id, comment) => {
-  
+  const config = {
+    headers: { Authorization: token },
+  }
   const response = await axios.post(
     `${baseUrl}/${id}/comments`,
-    {title: comment}
+    comment,
+    config,
   )
   return response.data
 }
 
-const getComments = async () => {
-  const response = await axios.get(`${baseUrl}/:id/comments`)
-  return response.data
+export default {
+  getAll,
+  create,
+  update,
+  deleteBlog,
+  setToken,
+  addComment,
 }
-
-export default { getAll, create, update, setToken, remove, addComment, getComments }
