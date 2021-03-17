68f47b3df10255b053a758da4a252083dccb5b89
Abhishek Ranjan <rabhishekranjan@gmail.com>
2021-03-17 15:13:15 +0530

    added path resolve to make heroku routing work

--- Bloglist-backend v2/app.js
+++ Bloglist-backend v2/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -15,14 +16,26 @@ const {requestLogger, unknownEndpoint, errorHandler} = require('./utils/middlewa
   mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   
   app.use(cors())
-  app.use(express.static('build'))
+  app.use(express.static('build'))  
   app.use(express.json())
+
   app.use(requestLogger)
   app.use('/api/blogs', blogsRouter)
   app.use('/api/blogs', commentsRouter)
   app.use('/api/users', usersRouter)
   app.use('/api/login', loginRouter)
 
+  
+  if (process.env.NODE_ENV === 'production') {
+    // Serve static files
+    app.use(express.static(path.resolve(__dirname, 'build', 'index.html')));
+  
+    app.get('*', (req, res, next) => {
+      // Serve index.html file if it doesn't recognize the route
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html')); // <- Here !
+    });
+  }
+  
 
   app.use(errorHandler)
   app.use(unknownEndpoint)
